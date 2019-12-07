6ea0c3763b042a0115714b16febb3462c3a68c1e
LeonGeroge <leongeroge@gmail.com>
2019-12-07 22:59:31 +0800

    init

---
+++ src/main/java/com/practice/spring/session/SpringSessionPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.practice.spring.session;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSessionPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSessionPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/practice/spring/session/SpringSessionPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.spring.session;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSessionPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
